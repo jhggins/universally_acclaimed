c6e577caf49012478ace9bf4333bb142f1535d60
Jared Huggins <j.hggins@gmail.com>
2019-05-11 18:11:58 -0400

    Add files via upload

--- universally_acclaimed.py
+++ universally_acclaimed.py
@@ -1,6 +1,13 @@
 # universally_acclaimed.py
-# generate a simple line chart of the number of Metacritic "universally
-# acclaimed" albums per release year, from 2000 to `current year`-1
+# Generate simple line charts of the number of Metacritic "universally acclaimed"
+# albums, for several major genres, per release year from 2000 to `current year`-1.
+
+# Any single album can belong to more than one genre, and compound genres are
+# split into their component genres (For example, a "Pop/Rock" album is a Pop
+# album and a Rock album).
+
+# Requires bs4, matplotlib, pandas, and numpy packages, all of which are
+# available in the Anaconda Distribution of Python3.
 
 import requests
 import re
